64be94c98d2c48bb7256816bd44b21913a40df37
yasminoliv3ira <134388213+yasminoliv3ira@users.noreply.github.com>
2023-11-09 08:52:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function potencia(){
+let n = parseInt (numero.value);
+let pote = n;
+if(n<0){
+    alert("numero negativo");
+}
+else if (n==0){
+     alert ("1")
+}
+
+else{
+    while(n>=1){
+    pote = math.pow(2,n);
+    n = n - 1;
+    alert(pote);
+    }
+    }
+}
+function soma(){
+    var n = parseInt(numero.value);
+    var soma = n;
+    while(n>=1){
+        soma= soma +
+    }
+}
